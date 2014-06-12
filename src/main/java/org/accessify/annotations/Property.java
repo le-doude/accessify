0770cd3ca35dd1f4f0611365188ee2c33416896a
le-doude <edouard.pelosi@gmail.com>
2014-06-12 11:07:13 +0900

    make Property an @interface as well

--- src/main/java/org/accessify/annotations/Property.java
+++ src/main/java/org/accessify/annotations/Property.java
@@ -6,7 +6,7 @@ import java.lang.annotation.*;
  * Created by edouard on 14/06/11.
  */
 
-public class Property {
+public @interface Property {
 
     @Target({ElementType.METHOD})
     @Retention(RetentionPolicy.RUNTIME)
