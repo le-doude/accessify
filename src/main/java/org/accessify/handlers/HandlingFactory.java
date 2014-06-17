5e3f9bce8e21e4bd3a3d6a63a9190279d9de4345
le-doude <edouard.pelosi@gmail.com>
2014-06-17 14:21:12 +0900

    handling factory basic implementation

--- src/main/java/org/accessify/handlers/HandlingFactory.java
+++ src/main/java/org/accessify/handlers/HandlingFactory.java
@@ -1,28 +1,36 @@
 package org.accessify.handlers;
 
 import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by edouard on 14/06/12.
  */
 public class HandlingFactory {
 
-
-    private HandlingFactory(){}
-
-    public static HandlingFactory get(){
-        return new HandlingFactory();
-    }
-
+    private static ConcurrentHashMap<String, HandlingFactory> factories = new ConcurrentHashMap<>();
     final HashMap<Class, ObjectHandler> handlers = new HashMap<>();
 
-    public void register(ObjectHandler handler) {
-        handlers.put(handler.handledType(), handler);
+    private HandlingFactory() {
+    }
+
+    public static HandlingFactory get(String instanceName) {
+        return factories.get(instanceName);
+    }
+
+    public static boolean create(String instanceName, ObjectHandler... handlers) {
+        return factories.putIfAbsent(instanceName, new HandlingFactory().register(handlers)) != null;
+    }
+
+    private HandlingFactory register(ObjectHandler... ohs) {
+        for (ObjectHandler handler : ohs) {
+            handlers.put(handler.handledType(), handler);
+        }
+        return this;
     }
 
     @SuppressWarnings("unchecked")
     public <T> ObjectHandler<T> handler(Class<T> clazz) {
         return (ObjectHandler<T>) handlers.get(clazz);
     }
-
 }
