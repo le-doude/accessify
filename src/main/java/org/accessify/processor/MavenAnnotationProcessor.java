232eb511028d43f9f339aa9b131aa1529962fa02
le-doude <edouard.pelosi@gmail.com>
2014-06-17 16:35:45 +0900

    getting classes in Maven annotation processor

--- src/main/java/org/accessify/processor/MavenAnnotationProcessor.java
+++ src/main/java/org/accessify/processor/MavenAnnotationProcessor.java
@@ -1,11 +1,17 @@
 package org.accessify.processor;
 
+import org.accessify.annotations.HandledType;
 import org.apache.commons.lang.NotImplementedException;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+import java.beans.IntrospectionException;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Set;
 
 /**
@@ -24,7 +30,36 @@ public class MavenAnnotationProcessor extends AbstractProcessor {
         }
     }
 
-    private boolean processImpl(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+    private boolean processImpl(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) throws ClassNotFoundException, IntrospectionException, IOException {
+
+        Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(HandledType.class);
+        ArrayList<Class> handledTypes = new ArrayList<>(elements.size());
+        Elements elementUtils = processingEnv.getElementUtils();
+        String className;
+        for (Element element : elements) {
+            if (element.getKind().isClass()) {
+                className = elementUtils.getBinaryName((TypeElement) element).toString();
+                Class<?> clazz = null;
+                try {
+                    clazz = Class.forName(className);
+                } catch (ClassNotFoundException e) {
+                    clazz = null;
+                }
+                if (clazz != null) {
+                    handledTypes.add(clazz);
+                }
+            }
+        }
+//        List<VelocityContext> contexts;
+//        CodeTemplateService service;
+//        for (Class type : handledTypes) {
+//            contexts = TemplatingContextsGenerator.generatePropertyHandlersContexts(type);
+////            TemplatingContextsGenerator.generateObjectHandlerContext(type, contexts);
+//            for (VelocityContext context : contexts) {
+//                service = new CodeTemplateService();
+//                service.writePropertyHandler(context, null);
+//            }
+//        }
         //TODO:IMplement me!
         throw new NotImplementedException();
     }
